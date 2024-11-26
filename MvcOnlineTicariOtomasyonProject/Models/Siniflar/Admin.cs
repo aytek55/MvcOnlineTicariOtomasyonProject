b84874c298b10a6ca51ba9a455d573db9c3832b6
ali Aytekin <ali_aytekin33@hotmail.com>
2024-11-26 15:08:25 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyonProject/Models/Siniflar/Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyonProject.Models.Siniflar
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string KullaniciAd { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string Sifre { get; set; }
+
+
+        [Column(TypeName = "char")]
+        [StringLength(1)]
+        public string Yetki { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyonProject/Models/Siniflar/FaturaKalem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyonProject.Models.Siniflar
+{
+    public class FaturaKalem
+    {
+        [Key]
+        public int FaturaKalemId { get; set; }
+
+        [Display(Name = "Açıklama")]
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string Aciklama { get; set; }
+
+        public int Miktar { get; set; }
+
+        [Display(Name = "Birim Fiyat")]
+        public decimal BirimFiyat { get; set; }
+        public decimal Tutar { get; set; }
+
+        public int Faturaid { get; set; }
+        public virtual Faturalar Faturalar { get; set; }
+    }
+}
